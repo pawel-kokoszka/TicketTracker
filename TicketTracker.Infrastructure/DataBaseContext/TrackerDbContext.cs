33dea1bac9233ccbec5e856b2093cb8199331c43
pawel-kokoszka <83219154+pawel-kokoszka@users.noreply.github.com>
2023-11-05 11:49:52 +0100

    Added class TrackerDbContext : DbContext - Added DbSet<Ticket> Tickets

---
+++ TicketTracker.Infrastructure/DataBaseContext/TrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketTracker.Domain.Entities;
+
+namespace TicketTracker.Infrastructure.DataBaseContext
+{
+    public class TrackerDbContext : DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+
+    }
+}
