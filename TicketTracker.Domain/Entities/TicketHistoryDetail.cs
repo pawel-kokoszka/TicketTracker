7928e159d660b0194bd79748aa5b93ebaad9a1d3
pawel-kokoszka <83219154+pawel-kokoszka@users.noreply.github.com>
2024-12-10 15:51:47 +0100

    Added migration to remove unused EditLockId column in TicketHistoryDetails;

--- TicketTracker.Domain/Entities/TicketHistoryDetail.cs
+++ TicketTracker.Domain/Entities/TicketHistoryDetail.cs
@@ -10,18 +10,12 @@ namespace TicketTracker.Domain.Entities
     {
         public int Id { get; set; }
         public int TicketHistoryId { get; set; }
-        public Guid EditLockId { get; set; }
         public bool IsDiscarded { get; set; }
         public string? TicketPropertyName { get; set; }
         public string? PropertyNewValue { get; set; }
-        //public string? PropertyNewDisplayValue { get; set; }
-
-        public string? PropertyOldValue { get; set; }
-        //public string? PropertyOldDisplayValue { get; set; }
-
+        public string? PropertyOldValue { get; set; }        
         public string? Comment { get; set; }
-        //trzeba dodać ticketlockid 
-
+        
         //ef
         public TicketHistory? TicketHistory { get; set; }
     }
