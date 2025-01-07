a4ede834c1a1f6e222e4ab392ce233ca9fb3934d
pawel-kokoszka <83219154+pawel-kokoszka@users.noreply.github.com>
2025-01-07 13:34:31 +0100

    Added flags in TicketDetailsDto to control displaying of resolved and completed dates; Added switch in GetTicketByIdQueryHandler to control those flags and timeLeft calculation;

--- TicketTracker.Application/Tickets/Queries/GetTicketById/GetTicketByIdQueryHandler.cs
+++ TicketTracker.Application/Tickets/Queries/GetTicketById/GetTicketByIdQueryHandler.cs
@@ -30,16 +30,106 @@ namespace TicketTracker.Application.Tickets.Queries.GetTicketById
 
             var slaTime = new TimeSpan(ticketSla.NumberOfDays,0,ticketSla.NumberOfMinutes,0);
 
-
-
-
             var createdDate = DateTime.Parse(ticketDetailsDto.DateCreated!);
 
-            var resolutionDate = createdDate + slaTime;
+            var resolutionDate = new DateTime();
+
+            var resolvedDate = new DateTime();
+
+            var timeLeft = new TimeSpan();
+
+            var ticketStatus = ticket.TicketStatusId;
+
+            switch (ticketStatus)
+            {
+                case 1:
+                case 2:
+                case 7:
+                    resolutionDate = createdDate + slaTime;
+                    timeLeft = resolutionDate - currentDate;
+                    ticketDetailsDto.ShowResolvedDate = false;
+                    ticketDetailsDto.ShowCompletedDate = false;
+                    ticketDetailsDto.ShowTicketSlaResolutionDate = true;
+                    ticketDetailsDto.ShowTicketSlaTimeLeft = true;
+                    ticketDetailsDto.ShowIsOverdue = true;
+                    break;
+                case 3:
+                case 4:
+                    resolutionDate = createdDate + slaTime;
+
+                    resolvedDate = (DateTime)ticket.DateSolved!;
+
+                    timeLeft = resolutionDate - resolvedDate;
+
+                    ticketDetailsDto.ShowResolvedDate = true;
+                    ticketDetailsDto.ShowCompletedDate = false;
+                    ticketDetailsDto.ShowTicketSlaResolutionDate = true;
+                    ticketDetailsDto.ShowTicketSlaTimeLeft = true;
+                    ticketDetailsDto.ShowIsOverdue = true;
+                    break;
+                case 5:
+                case 6:
+                    resolutionDate = createdDate + slaTime;
+
+                    resolvedDate = (DateTime)ticket.DateSolved!;
+
+                    timeLeft = resolutionDate - resolvedDate;
+
+                    ticketDetailsDto.ShowResolvedDate = true;
+                    ticketDetailsDto.ShowCompletedDate = true;
+                    ticketDetailsDto.ShowTicketSlaResolutionDate = true;
+                    ticketDetailsDto.ShowTicketSlaTimeLeft = true;
+                    ticketDetailsDto.ShowIsOverdue = true;
+                    break;
+                case 8:
+                    resolutionDate = currentDate;
+                    timeLeft = currentDate - currentDate;
+
+                    ticketDetailsDto.ShowResolvedDate = false;
+                    ticketDetailsDto.ShowCompletedDate = false;
+                    ticketDetailsDto.ShowTicketSlaResolutionDate = false;
+                    ticketDetailsDto.ShowTicketSlaTimeLeft = false;
+                    ticketDetailsDto.ShowIsOverdue = false;
+                    break;
+            }
+
+            //spr. status
+            //1 jesli open || wip to || reopened 
+            //obl. resolution date i wklej ją do TicketSlaResolutionDate i TicketSlaTimeLeft
+            // display resolvedDate = false
+            // display completedDate = false
+            // display TicketSlaTimeLeft = true
+            // display TicketSlaResolutionDate = true
+            // display IsOverdue = true
+
+            //2 jesli resolved || pending 
+            //obl. resolution date i wklej ją do TicketSlaResolutionDate 
+            //w TicketDateResolved wklej DateResolved, a w TicketSlaTimeLeft obliczyć czas całkowity od otwarcia do sla, i odjąć od niego czas otawrcia do reslved 
+            // display resolvedDate = true
+            // display completedDate = false
+            // display TicketSlaTimeLeft = true
+            // display TicketSlaResolutionDate = true
+            // display IsOverdue = true
+
+            //3 jesli completed || closed 
+            // to samo co w 2 i:
+            // w ticketdDateCompleted wklej DateCompleted, 
+            // display resolvedDate = true
+            // display completedDate = true               
+            // display TicketSlaTimeLeft = true
+            // display TicketSlaResolutionDate = true
+            // display IsOverdue = true
+
+            //4 jesli rejected:
+            // display resolvedDate = false
+            // display completedDate = false
+            // display TicketSlaTimeLeft = false
+            // display TicketSlaResolutionDate = false
+            // display IsOverdue = false
+            // display TicketSlaTimeLeft = false
+            // display TicketSlaResolutionDate = false
+            // display IsOverdue = false
             
-            var timeLeft =  resolutionDate - currentDate;
-
-
             ticketDetailsDto.TicketSlaResolutionDate = resolutionDate.ToString("yyyy-MM-dd HH:mm"); ;
 
             ticketDetailsDto.TicketSlaTimeLeft = $"{timeLeft.Days} days, {timeLeft.Hours} hours, {timeLeft.Minutes} minutes";
@@ -53,3 +143,12 @@ namespace TicketTracker.Application.Tickets.Queries.GetTicketById
         }
     }
 }
+//Id Name
+//1	Opened
+//2	Work in Progress
+//3	Resolved
+//4	Pending
+//5	Completed
+//6	Closed
+//7	Reopened
+//8	Rejected
