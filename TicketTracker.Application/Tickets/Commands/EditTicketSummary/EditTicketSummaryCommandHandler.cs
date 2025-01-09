56d0dbf65c0e842671c0dca6dd18db316368b43d
pawel-kokoszka <83219154+pawel-kokoszka@users.noreply.github.com>
2025-01-09 10:15:19 +0100

    Method name typo correction for WriteChangedPropertiesToTicket();

--- TicketTracker.Application/Tickets/Commands/EditTicketSummary/EditTicketSummaryCommandHandler.cs
+++ TicketTracker.Application/Tickets/Commands/EditTicketSummary/EditTicketSummaryCommandHandler.cs
@@ -34,7 +34,7 @@ namespace TicketTracker.Application.Tickets.Commands.EditTicketSummary
             var ticketEditedData = request.TicketHistory;
 
 
-            var numberOfChangedProperties = WriteChangedProperitesToTicket(ticketOryginalData, ticketEditedData!.HistoryDetails!);
+            var numberOfChangedProperties = WriteChangedPropertiesToTicket(ticketOryginalData, ticketEditedData!.HistoryDetails!);
             if (numberOfChangedProperties == 0)
             {
                 return Unit.Value;
@@ -100,7 +100,7 @@ namespace TicketTracker.Application.Tickets.Commands.EditTicketSummary
             return Unit.Value;
         }
 
-        private int WriteChangedProperitesToTicket(Ticket ticketOryginalData, List<TicketHistoryDetailDto> changedProperties)
+        private int WriteChangedPropertiesToTicket(Ticket ticketOryginalData, List<TicketHistoryDetailDto> changedProperties)
         {
             int numberOfChanges = 0;
 
